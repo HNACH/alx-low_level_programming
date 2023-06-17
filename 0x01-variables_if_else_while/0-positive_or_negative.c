84921644bd1b575daf60d4264dad6cdf718b5ac0
HNACH <medhnach2022@gmail.com>
2023-06-16 18:49:08 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
   *main - Entry point
-  THIS CODE CHECKS IS N  POSITIVE OR NEGATIVE
+  *THIS CODE CHECKS IS N  POSITIVE OR NEGATIVE
   *Return: always 0
   */
 int main(void)
@@ -20,7 +20,7 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}	
+	}
 	else
 	{
 		printf("%d is negative\n", n);
