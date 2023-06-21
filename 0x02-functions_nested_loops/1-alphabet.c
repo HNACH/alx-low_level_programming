1026ab5206af129080b4352d13949e179c2c5ed2
HNACH <medhnach2022@gmail.com>
2023-06-20 17:52:45 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
-/** 
-  *Prints alphabets in lowercase.
-  */
-void print_alphabet(void)
 
+/**
+ * print_alphabet - Prints alphabets in lowercase.
+ */
+void print_alphabet(void)
 {
 	char letter;
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
 
-		_putchar('\n');
+	_putchar('\n');
 }
